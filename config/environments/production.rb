6eb006ff90cf7c67303ebb3c42627a8963e4e4fd
bhanuteja24 <bhanuteja24@gmail.com>
2016-10-07 00:42:37 -0500

    settings added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,9 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'software-management-app.herokuapp.com', :protocol => 'https'}
+
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
